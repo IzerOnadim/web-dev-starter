fe7928c420897a7f14ba62b0895c73054e58e2cd
Izer <izer.onadim19@imperial.ac.uk>
2020-06-28 21:39:18 +0100

    Switch home.js to strict mode

--- home.js
+++ home.js
@@ -1,7 +1,20 @@
+"use strict";
+
 console.log("Hello World!");
 
 let name = "World";
 let title = `Hello ${name}!`;
 
+let person = {
+    name: "Izer",
+    age: 32,
+};
+
+function incrementAge(p) {
+    p.age++;
+}
+
 changeDiscount(25);
-showMessage(title);
+
+incrementAge(person);
+showMessage(`Hello ${person.name}`);
